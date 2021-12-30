56ff2a6b2d7d13e9dd4a42d9ddb86cb8a7343a98
Muhammad Fauzi <muhammadfauzi.2.mf52@gmail.com>
2021-12-30 11:59:29 +0700

    update

--- components/Description/index.js
+++ components/Description/index.js
@@ -9,7 +9,7 @@ const Description = () => {
         <div className="flex flex-col items-start w-80">
           <div className="flex justify-center w-24 h-24 bg-purple-700 rounded-lg text-white items-center mb-4">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
             </svg>
           </div>
           <p className="font-body font-bold text-lg text-purple-700">Employer giving project briefing</p>
