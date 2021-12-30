92fe8db13dfc5d4fab6857eb79c19c9c09362eb8
Muhammad Fauzi <muhammadfauzi.2.mf52@gmail.com>
2021-12-30 11:51:14 +0700

    update

--- components/Description/index.js
+++ components/Description/index.js
@@ -19,7 +19,7 @@ const Description = () => {
         <div className="flex flex-col items-start w-80">
           <div className="flex justify-center w-24 h-24 bg-purple-700 rounded-lg text-white items-center mb-4">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 21h7a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v11m0 5l4.879-4.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242z" />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 21h7a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v11m0 5l4.879-4.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242z" />
             </svg>
           </div>
           <p className="font-body font-bold text-lg text-purple-700">Workers looking for references</p>
@@ -29,7 +29,7 @@ const Description = () => {
         <div className="flex flex-col items-start w-80">
           <div className="flex justify-center w-24 h-24 bg-purple-700 rounded-lg text-white items-center mb-4">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
             </svg>
           </div>
           <p className="font-body font-bold text-lg text-purple-700">Workers doing design work</p>
@@ -39,7 +39,7 @@ const Description = () => {
         <div className="flex flex-col items-start w-80">
           <div className="flex justify-center w-24 h-24 bg-purple-700 rounded-lg text-white items-center mb-4">
            <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
           </div>
           <p className="font-body font-bold text-lg text-purple-700">Feedback from client</p>
