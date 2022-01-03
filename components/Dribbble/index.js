3e4dd858eb4c5066b30cbb69d22df7f506d9be73
Muhammad Fauzi <muhammadfauzi.2.mf52@gmail.com>
2022-01-03 23:03:14 +0700

    down

--- components/Dribbble/index.js
+++ components/Dribbble/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 
 
 const Dribbble = ({data}) => {
+  const desc = `<p>${data.description}</p>`
   return (
    <div className="flex flex-col justify-center my-10">
       <h1 className="flex justify-center items-center font-display text-4xl text-purple-700 pb-10">Project and Exploration</h1>
@@ -10,14 +11,12 @@ const Dribbble = ({data}) => {
       <div className="flex flex-wrap  justify-center gap-8 ">
       {data.map((data) => {
         return(
-            <div key={data.id} className="w-96 border border-purple-300 p-4 rounded-lg ">
+            <div key={data.id} className="w-96 border border-purple-300 p-4 rounded-lg">
               <img src={data.images.normal} alt="images-for-dribbble" className="rounded-lg"/>
-              <h1 dangerouslySetInnerHTML={{
-                __html:`${data.title}`
-              }} className='font-body  font-bold text-purple-700 text-lg  py-2' ></h1>
-              <p dangerouslySetInnerHTML={{
+              <h1 className="font-body  font-bold text-purple-700 text-lg  py-2 ">{data.title}</h1>
+              <div dangerouslySetInnerHTML={{
                 __html: `${data.description}`
-              }} className='font-body text-md text-gray-500'/>
+              }} className='font-body text-md text-gray-500'></div>
             </div>
 
         )
